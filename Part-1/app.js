51ec1324a8018909861b51819fbbf58460005a81
Justin Bak <jbak@mtech.edu>
2018-03-11 20:45:31 -0600

    refactored more things

--- Part-1/app.js
+++ Part-1/app.js
@@ -1,14 +1,18 @@
 (function() {
     console.log("buttons can't reach this :(");    
-})()
+    function buttonPressed() {
+        var texts = ['Text things', 'More text things'];
 
-var toggle = true
-function changeText() {
-    if (toggle) {
-        toggle = false;
-        return "things change"
-    }else {
-        toggle = true;
-        return "texty stuff"
+        var textField = document.querySelector('#demo');
+
+        var text = textField.innerHTML;
+
+        text = (text === texts[0]) ? texts[1] : texts[0];
+
+        textField.innerHTML = text;
     }
-}
+
+    //event hanlder thingy
+    document.querySelector('#Button').addEventListener('click',
+        buttonPressed);
+})()
